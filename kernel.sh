5308d4dfd72a573b16b6e127be540ca76f515b06
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-12-24 18:57:36 -0300

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -26,7 +26,6 @@ JOBS=$(nproc --all)
 export KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
 function clone() {
-    message "Cloning dependencies..."
     if ! [ -a AnyKernel3 ]; then
         git clone --depth=1 https://github.com/MarvelMathesh/AnyKernel3 -b land AnyKernel3
     fi
@@ -34,20 +33,17 @@ function clone() {
         git clone --depth=1 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android12-release
     fi
     if ! [ -a clang ]; then
-        git clone --depth=1 https://github.com/physwizz/compiler compiler
+        git clone --depth=1 https://github.com/physwizz/compiler -b main compiler
     fi
-    https://github.com/physwizz/compiler
 }
 
 function compile() {
-    message "Compiling kernel..."
     if [ -a out ]; then
         rm -rf out
     fi
-    make clean && make mrproper
     export ANDROID_MAJOR_VERSION=r
-    export CROSS_COMPILE=/compiler/bin/aarch64-linux-android-
-    export KERNEL_LLVM_BIN=/linux-x86/clang-r416183b/bin/
+    export BUILD_CROSS_COMPILE=$(pwd)/compiler/bin/aarch64-linux-android-
+    export KERNEL_LLVM_BIN=$(pwd)/linux-x86/clang-r416183b/bin/
     make -C $(pwd) O=out $KERNEL_MAKE_ENV REAL_CC=$KERNEL_LLVM_BIN KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y vendor/m23xq_eur_open_defconfig -j"$JOBS" \
         "${MAKE_FLAGS[@]}"
     make -C $(pwd) O=out $KERNEL_MAKE_ENV REAL_CC=$KERNEL_LLVM_BIN KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j"$JOBS" \
@@ -55,7 +51,6 @@ function compile() {
 }
 
 function repack() {
-    message "Repacking kernel..."
     if ! [ -a AnyKernel3/Image.gz-dtb ]; then
         cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
     fi
